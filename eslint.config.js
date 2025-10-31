b07a40d2c249d6973ddce12e4ed15b626e18d45f
file not found <voodoologic@users.noreply.github.com>
2025-10-30 19:20:42 -0700

    cs linting

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['your-existing-config', 'plugin:ember/recommended'],
+  plugins: ['ember'],
+  env: {
+    browser: true,
+  },
+  rules: {
+    // You can add any additional custom rules or override existing ones here
+  },
+};;
