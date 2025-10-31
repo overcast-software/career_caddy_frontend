debdffc003dab123b0375448ae8d0bae937ed797
file not found <voodoologic@users.noreply.github.com>
2025-10-30 19:48:13 -0700

    lint work

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,8 @@
 'use strict';
 
 module.exports = {
-  // Include configurations directly in the flat config array
-  plugins: [
-    'stylelint-config-standard',
-    'stylelint-config-recommended'
-  ],
-  // Add rules and settings as needed
+  extends: ['stylelint-config-standard'],
   rules: {
-    // Your ESLint rules here
+    // Your Stylelint rules here
   }
 };
