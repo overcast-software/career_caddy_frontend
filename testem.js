9ea8a8e83c606e1193e89541b0850130dc216298
file not found <voodoologic@users.noreply.github.com>
2026-04-05 19:29:40 -0700

    Use Chromium launcher name to match installed binary

--- testem.js
+++ testem.js
@@ -3,24 +3,20 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_ci: ['Chromium'],
+  launch_in_dev: ['Chromium'],
   browser_start_timeout: 120,
   browser_args: {
-    Chrome: {
+    Chromium: {
       ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
+        '--no-sandbox',
         '--headless',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900',
-      ].filter(Boolean),
-    },
-    Firefox: {
-      ci: ['-headless', '-width=1440', '-height=900'],
+      ],
     },
   },
 };
