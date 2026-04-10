8aa528277eede1c3c16be2785b99a971d4b39b63
file not found <voodoologic@users.noreply.github.com>
2026-04-10 04:56:13 -0700

    fix Resumes::Title::Form to use @tracked intermediary values
    Prevents lost characters on fast typing by using tracked properties
    instead of direct model mutation. Also fixes incorrect name attributes.

--- app/components/resumes/title/form.js
+++ app/components/resumes/title/form.js
@@ -1,14 +1,36 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ResumesTitleForm extends Component {
+  @tracked _title;
+  @tracked _name;
+  @tracked _notes;
+
+  get title() {
+    return this._title ?? this.args.resume?.title;
+  }
+
+  get name() {
+    return this._name ?? this.args.resume?.name;
+  }
+
+  get notes() {
+    return this._notes ?? this.args.resume?.notes;
+  }
+
   @action updateTitle(event) {
-    this.args.resume.title = event.target.value;
+    this._title = event.target.value;
+    this.args.resume.title = this._title;
   }
+
   @action updateName(event) {
-    this.args.resume.name = event.target.value;
+    this._name = event.target.value;
+    this.args.resume.name = this._name;
   }
+
   @action updateNotes(event) {
-    this.args.resume.notes = event.target.value;
+    this._notes = event.target.value;
+    this.args.resume.notes = this._notes;
   }
 }
